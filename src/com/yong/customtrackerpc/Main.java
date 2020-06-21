7aa1adf022b9aaf691888113d1eb94e31ee9a94b
yymin1022 <yymin1022@gmail.com>
2020-06-21 20:38:09 +0900

    Add ParcelData Loading Function

--- src/com/yong/customtrackerpc/Main.java
+++ src/com/yong/customtrackerpc/Main.java
@@ -1,18 +1,40 @@
 package com.yong.customtrackerpc;
 
-import java.util.Locale;
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.Source;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String trackUrl = "http://defcon.or.kr:90/get%sInfo?trackNum=%d";
-        int trackNum = Integer.valueOf(args[1]);
+    public static void main(String[] args){
+        String defaultkUrl = "http://defcon.or.kr:90/get%sInfo?trackNum=%s";
+        String trackUrl = "";
+        String trackNum = args[1];
         if(args[0].equals("Track")){
-            trackUrl = String.format(Locale.getDefault(), trackUrl, "Track", trackNum);
+            trackUrl = String.format(defaultkUrl, "Track", trackNum);
+            getParcelData(trackUrl);
         }else if(args[0].equals("Parcel")){
-            trackUrl = String.format(Locale.getDefault(), trackUrl, "Parcel", trackNum);
+            trackUrl = String.format(defaultkUrl, "Parcel", trackNum);
+            getTrackData(trackUrl);
         }
+    }
 
+    static void getTrackData(String strURL){
 
+    }
+
+    static void getParcelData(String strURL){
+        Source htmlSource = null;
+        try {
+            htmlSource = new Source(new URL(strURL));
+            List<Element> tagList = htmlSource.getAllElements();
+
+            System.out.println(htmlSource);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
