28058f2671e89ea53a9f7c0106ab901ab7c3eadd
yymin1022 <yymin1022@gmail.com>
2020-06-21 22:08:59 +0900

    Improved Printing Style of Track Result

--- src/com/yong/customtrackerpc/Main.java
+++ src/com/yong/customtrackerpc/Main.java
@@ -5,7 +5,9 @@ import net.htmlparser.jericho.Source;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
 
@@ -15,21 +17,35 @@ public class Main {
         String trackNum = args[1];
         if(args[0].equals("Track")){
             trackUrl = String.format(defaultkUrl, "Track", trackNum);
-            getParcelData(trackUrl);
+            getTrackData(trackUrl);
         }else if(args[0].equals("Parcel")){
             trackUrl = String.format(defaultkUrl, "Parcel", trackNum);
-            getTrackData(trackUrl);
+            getParcelData(trackUrl);
         }
     }
 
     static void getTrackData(String strURL){
+        try {
+            Source pageResult = new Source(new URL(strURL));
+            String[] steps = pageResult.toString().split("&&\n");
 
+            for(String step : steps){
+                SimpleDateFormat formatBefore = new SimpleDateFormat("yyyyMMddHHmmss");
+                Date date = formatBefore.parse(step.split(":")[0]);
+                SimpleDateFormat formatAfter = new SimpleDateFormat("yyyy. MM. dd. HH:mm");
+                String newDate = formatAfter.format(date);
+
+                System.out.println(String.format("%s :%s", newDate, step.split(":")[1]));
+            }
+
+        } catch (IOException | ParseException exception) {
+            exception.printStackTrace();
+        }
     }
 
     static void getParcelData(String strURL){
         try {
             Source htmlSource = new Source(new URL(strURL));
-            List<Element> tagList = htmlSource.getAllElements();
 
             System.out.println(htmlSource);
         } catch (IOException exception) {
