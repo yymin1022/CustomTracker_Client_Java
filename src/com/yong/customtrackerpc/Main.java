288d9a51f355190883091a219fea6eb77907a6c0
yymin1022 <yymin1022@gmail.com>
2020-06-21 18:50:45 +0900

    Add URL Parameter related Code

--- src/com/yong/customtrackerpc/Main.java
+++ src/com/yong/customtrackerpc/Main.java
@@ -1,7 +1,18 @@
 package com.yong.customtrackerpc;
 
+import java.util.Locale;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String trackUrl = "http://defcon.or.kr:90/get%sInfo?trackNum=%d";
+        int trackNum = Integer.valueOf(args[1]);
+        if(args[0].equals("Track")){
+            trackUrl = String.format(Locale.getDefault(), trackUrl, "Track", trackNum);
+        }else if(args[0].equals("Parcel")){
+            trackUrl = String.format(Locale.getDefault(), trackUrl, "Parcel", trackNum);
+        }
+
+        
     }
 }
